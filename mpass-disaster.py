2b4e4293111412e0c583db4c69b0618e85d3b263
Rob Meijer <rob@noemail>
2022-09-23 00:24:53 +0200

    merkle tree to flat hash

--- mpass-disaster.py
+++ mpass-disaster.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import json
 from sys import argv
 from base58 import b58decode, b58encode
 from ellipticcurve.privateKey import PrivateKey
@@ -14,6 +15,7 @@ from hashlib import sha256, sha512
 import hashlib
 from lighthive.client import Client
 from lighthive.exceptions import RPCNodeException
+from lighthive.datastructures import Operation
 
 def disasterkey_to_wif(pubkey):
     return "CZD" + b58encode(b'\xbb' + pubkey + sha256(sha256(b'\xbb' + pubkey).digest()).digest()[:4]).decode("ascii")
@@ -44,19 +46,17 @@ class HiveAccount:
     def __init__(self, username, password):
         self.scope = "disaster"
         self.keylen = 32
+        self.username = username
         self.owner = key_from_creds(username, "owner", password)
-        self.posting = key_from_creds(username, "posting", password)
+        self.active = key_from_creds(username, "active", password)
         self.disaster = key_from_creds(username, self.scope, password)
-        postingkey = PrivateKey.fromString(hexlify(self.posting))
-        posting_public = pubkey_to_compressed(postingkey.publicKey())
-        client = Client()
-        b58key = "STM" + b58encode(posting_public).decode("ascii")
-        try:
-            keyrefs = client.get_key_references([b58key])
-            if keyrefs[0][0] != username:
-                print("ERROR: User and password don't match with HIVE account, ignore for now")
-        except RPCNodeException:
-            print("ERROR: Invalid posting key according to lighthive, ignore for now")
+        activekey = PrivateKey.fromString(hexlify(self.active))
+        active_public = pubkey_to_compressed(activekey.publicKey())
+        self.client = Client()
+        b58key = "STM" + b58encode(active_public).decode("ascii")
+        keyrefs = self.client.get_key_references([b58key])
+        if not keyrefs[0] or keyrefs[0][0] != username:
+            raise RuntimeError("ERROR: User and password don't match with HIVE account.")
     def disaster_pubkey(self):
         # Derive a salt for hashing operations from the private key
         hashing_salt = crypto_kdf_derive_from_key(self.keylen, 0, self.scope, self.disaster)
@@ -69,14 +69,10 @@ class HiveAccount:
                                       digest_size=self.keylen,
                                       key=hashing_salt,
                                       encoder=RawEncoder)
-        #FIXME: This could be a flat hash instead of a merkletree.
-        # Reduce the 32 pairs of byte-signing public keys to a single merkle-tree root
-        while len(chunks) > 1:
-            chunks = [blake2b(chunks[x*2] + chunks[x*2+1],
-                              digest_size=self.keylen,
-                              key=hashing_salt,
-                              encoder=RawEncoder) for x in range(0, len(chunks)//2)]
-        return chunks[0]
+        return blake2b(b"".join(chunks),
+                       digest_size=self.keylen,
+                       key=hashing_salt,
+                       encoder=RawEncoder)
 
     def paperwallet(self):
         return bip39.encode_bytes(self.disaster)
@@ -86,6 +82,26 @@ class HiveAccount:
         sig =  ecdsa.Ecdsa.sign(data.decode("latin1"), ecdsa_signingkey)
         return sig.toDer(withRecoveryId=True)
 
+    def update_account_json(self):
+        account_obj = json.loads(self.client.get_accounts([self.username])[0]["json_metadata"])
+        pubkey = self.disaster_pubkey()
+        sig = self.owner_sign(pubkey)
+        keyinfo = {}
+        keyinfo["key"] = disasterkey_to_wif(pubkey)
+        keyinfo["sig"] = b58encode(sig).decode("latin1")
+        account_obj["coinzdense_disaster_recovery"] = keyinfo
+        newjson = json.dumps(account_obj)
+        active = to_priv_repr(self.active)
+        c = Client(keys=[active])
+        ops = [
+            Operation('account_update', 
+                      {
+                        'account': self.username,
+                        'json_metadata': newjson
+                      }
+                     )
+        ]
+        c.broadcast(ops)
 
 def main():
     if len(argv) < 2:
@@ -94,12 +110,8 @@ def main():
     username = argv[1]
     password = getpass("Password for " + username + ": ")
     account = HiveAccount(username, password=password)
-    print("Disaster pubkey:")
-    pubkey = account.disaster_pubkey()
-    print(" *", disasterkey_to_wif(pubkey))
-    sig = account.owner_sign(pubkey)
-    print("ECDSA Signature:")
-    print(" *", b58encode(sig).decode("latin1"))
+    account.update_account_json()
+    print("Registered disaster recovery key")
 
 if __name__ == "__main__":
     main()
